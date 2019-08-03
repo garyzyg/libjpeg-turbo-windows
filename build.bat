943fa9686f7c4a3e49fb8a403465e500af2c043b
Gary Zhang <492828+garyzyg@users.noreply.github.com>
2019-08-03 16:15:02 +0800

    clean up script and switch compiler to LLVM

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+SET SED=C:\msys64\usr\bin\sed.exe
+
 PUSHD .
 FOR %%I IN (C:\WinDDK\7600.16385.1) DO CALL %%I\bin\setenv.bat %%I fre %Platform% WIN7 no_oacr
 POPD
@@ -9,9 +11,21 @@ SET Include=%Include%;%CRT_INC_PATH%
 
 SET Path=%CD%;C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin;%Path%
 
-FOR /F "DELIMS=" %%I IN ('WHERE cmake.exe') DO FOR /F "DELIMS=" %%J IN ('DIR /B /S "%%~dpI..\Windows-MSVC.cmake"') DO FOR %%K IN (
-"/O2 /Ob2		/O1 /GL /GS-"
-"\${_RTC1}		 "
-) DO FOR /F "TOKENS=1,* DELIMS=	" %%L IN (%%K) DO C:\msys64\usr\bin\sed.exe "s@%%L@%%M@" -i "%%J"
 
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DWITH_CRT_DLL=1 -DCMAKE_EXE_LINKER_FLAGS=/MANIFEST:NO %CD%& FOR %%I IN (*.1) DO FOR /F "TOKENS=2 DELIMS=( " %%J IN ('FIND "add_executable" CMakeLists.txt ^| FIND "%%~nI"') DO nmake %%J VERBOSE=1
+FOR %%I IN (
+clang-cl.exe
+lld-link.exe
+llvm-lib.exe
+) DO ^
+FOR /F "TOKENS=2 DELIMS=-" %%J IN ('ECHO %%I') DO ^
+FOR /F "DELIMS=" %%K IN ('WHERE %%I') DO ^
+SET Path=%%~dpK;%Path%& MKLINK /H "%%~dpK%%J" "%%K"
+
+IF %Platform%==x64 (SET M64=-m64) ELSE SET M64=-m32
+
+FOR /F "DELIMS=" %%I IN ('WHERE cmake.exe') DO FOR /F "DELIMS=" %%J IN ('DIR /B /S "%%~dpI..\Windows-MSVC.cmake"') DO FOR %%K IN (
+"/O2 /Ob2		/O1 -flto /GS- %M64%"
+"\${_RTC1}		%M64%"
+) DO FOR /F "TOKENS=1,* DELIMS=	" %%L IN (%%K) DO %SED% "s@%%L@%%M@" -i "%%J"
+
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="%M64%" -DWITH_CRT_DLL=1 -DCMAKE_EXE_LINKER_FLAGS=/MANIFEST:NO %CD%& FOR %%I IN (*.1) DO FOR /F "TOKENS=2 DELIMS=( " %%J IN ('FIND "add_executable" CMakeLists.txt ^| FIND "%%~nI"') DO nmake %%J VERBOSE=1
